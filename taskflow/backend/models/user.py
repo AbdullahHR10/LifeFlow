8332461a0db995dcad3b99c3e8c4f4e3f5df3467
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-04-09 07:51:10 +0200

    Add tasks relationship to User

--- taskflow/backend/models/user.py
+++ taskflow/backend/models/user.py
@@ -2,6 +2,7 @@
 from .base_model import BaseModel
 from flask_login import UserMixin
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, UserMixin):
@@ -10,3 +11,5 @@ class User(BaseModel, UserMixin):
     name = Column(String(30), nullable=False)
     email = Column(String(255), nullable=False)
     password = Column(String(255), nullable=False)
+
+    tasks = relationship("Task", back_populates="user")
