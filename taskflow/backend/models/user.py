c681b09b742626392aee2b32f44223a450aae071
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-04-15 17:18:42 +0200

    Implement password hashing with Bcrypt in User model

--- taskflow/backend/models/user.py
+++ taskflow/backend/models/user.py
@@ -3,6 +3,7 @@ from .base_model import BaseModel
 from flask_login import UserMixin
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from backend.extensions import bcrypt
 
 
 class User(BaseModel, UserMixin):
@@ -10,8 +11,22 @@ class User(BaseModel, UserMixin):
     __tablename__ = "users"
     name = Column(String(30), nullable=False)
     email = Column(String(255), nullable=False)
-    password = Column(String(255), nullable=False)
+    _password = Column("password", String(255), nullable=False)
 
     tasks = relationship("Task", back_populates="user")
     habits = relationship("Habit", back_populates="user")
     notes = relationship("Note", back_populates="user")
+
+    @property
+    def password(self):
+        """Raises an error when someone tries to read the raw password."""
+        raise AttributeError("Password is write-only.")
+
+    @password.setter
+    def password(self, plain_password):
+        """Hashes and sets the password."""
+        self._password = bcrypt.generate_password_hash(plain_password).decode('utf-8')
+
+    def check_password(self, input_password):
+        """Checks the user's password."""
+        return bcrypt.check_password_hash(self._password, input_password)
