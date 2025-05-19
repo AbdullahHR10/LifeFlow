2f78c0d0adb746de7285200f6413790fc4382209
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-05-19 18:23:33 +0300

    Add application's configuration

---
+++ taskflow/backend/config.py
@@ -0,0 +1,19 @@
+"""Module that contains the Config class."""
+import os
+from redis import Redis
+import logging
+
+class Config():
+    """Configuration for the Flask application."""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SESSION_TYPE = 'redis'
+    SESSION_PERMANENT = False
+    SESSION_USE_SIGNER = True
+    SESSION_KEY_PREFIX = 'taskflow:'
+    SESSION_REDIS = Redis(host='localhost', port=6379)
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s - %(levelname)s - %(message)s"
+        )
