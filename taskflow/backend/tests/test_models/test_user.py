f8e06ba5299e867a4b81fd0ced14eb88fc6541b8
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-05-20 15:54:20 +0300

    Polish tests docstrings

--- taskflow/backend/tests/test_models/test_user.py
+++ taskflow/backend/tests/test_models/test_user.py
@@ -3,7 +3,6 @@
 import unittest
 from backend import create_app, db
 from backend.models.user import User
-from datetime import datetime
 
 
 class TestUserClass(unittest.TestCase):
@@ -31,44 +30,54 @@ class TestUserClass(unittest.TestCase):
         self.app_context.pop()
 
     def test_id(self):
-        """Tests the User class's id"""
+        """Tests the user's id"""
         self.assertIsInstance(self.user.id, str)
         self.assertEqual(self.user.id, "f47ac10b-58cc-4372-a567-0e02b2c3d479")
 
     def test_created_at(self):
-        """Tests the User class's created_at"""
+        """Tests the user's created_at"""
         self.assertIsInstance(self.user.created_at, str)
         self.assertEqual(self.user.created_at, "2025-05-01 22:00:00")
 
     def test_updated_at(self):
-        """Tests the User class's updated_at"""
+        """Tests the user's updated_at"""
         self.assertIsInstance(self.user.updated_at, str)
         self.assertEqual(self.user.updated_at, "2025-05-02 18:12:00")
 
     def test_name(self):
-        """Tests the User class's name"""
+        """Tests the user's name"""
         self.assertIsInstance(self.user.name, str)
         self.assertEqual(self.user.name, "testuser")
 
     def test_name_long(self):
-        """Tests the User class's name if it's too long."""
+        """
+        Tests the user's name if it's too long.
+
+        Raises:
+            ValueError: if the name is too long.
+        """
         with self.assertRaises(ValueError) as context:
             self.user.name = "a" * 31
         self.assertIn("name must be between 3 and 30 characters", str(context.exception))
 
     def test_name_short(self):
-        """Tests the User class's name if it's too short."""
+        """
+        Tests the user's name if it's too short.
+
+        Raises:
+            ValueError: if the name is too short.
+        """
         with self.assertRaises(ValueError) as context:
             self.user.name = "aa"
         self.assertIn("name must be between 3 and 30 characters", str(context.exception))
 
     def test_email(self):
-        """Tests the User class's email"""
+        """Tests the users's email"""
         self.assertIsInstance(self.user.email, str)
         self.assertEqual(self.user.email, "testemail@example.com")
 
     def test_password(self):
-        """Tests the User class's password"""
+        """Tests the users's password"""
         self.assertIsInstance(self.user._password, str)
         self.assertTrue(self.user.check_password("123456"))
         self.assertFalse(self.user.check_password("wrongpassword"))
