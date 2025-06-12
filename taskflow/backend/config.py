00cbc80b034434296196cfdf55d6ff61f67f473d
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-06-12 08:25:30 +0300

    Delete redis from the config classes

--- taskflow/backend/config.py
+++ taskflow/backend/config.py
@@ -1,6 +1,5 @@
 """Module that contains the Config class."""
 import os
-from redis import Redis
 import logging
 
 class Config():
@@ -8,11 +7,7 @@ class Config():
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SESSION_TYPE = 'redis'
-    SESSION_PERMANENT = False
-    SESSION_USE_SIGNER = True
-    SESSION_KEY_PREFIX = 'taskflow:'
-    SESSION_REDIS = Redis(host='localhost', port=6379)
+
     logging.basicConfig(
         level=logging.DEBUG,
         format="%(asctime)s - %(levelname)s - %(message)s"
@@ -23,7 +18,3 @@ class TestConfig():
     TESTING = True
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SESSION_TYPE = "redis"
-    SESSION_PERMANENT = False
-    SESSION_KEY_PREFIX = "taskflow_test:"
-    SESSION_REDIS = Redis(host="localhost", port=6379)
