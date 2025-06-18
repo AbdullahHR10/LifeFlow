530ad0f0759059cc79f42dfada4192a4d0547905
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-06-18 15:37:53 +0300

    Add background_color column

--- taskflow/backend/models/note.py
+++ taskflow/backend/models/note.py
@@ -1,14 +1,16 @@
 """Module that contains the Note class."""
 from .base_model import BaseModel
-from sqlalchemy import Column, String, Text, ForeignKey
+from sqlalchemy import Column, String, Text, ForeignKey, Enum as SqlEnum
 from sqlalchemy.orm import relationship
+from ..utils.enums import BackgroundColor
 
 
 class Note(BaseModel):
     """Represents a note in the application."""
     __tablename__ = "notes"
     title = Column(String(30), nullable=False)
-    content = Column(Text, nullable=False) 
+    content = Column(Text, nullable=False)
+    background_color = Column(SqlEnum(BackgroundColor), nullable=True)
 
     user_id = Column(String(36), ForeignKey('users.id'), nullable=False)
     user = relationship("User", back_populates="notes")
