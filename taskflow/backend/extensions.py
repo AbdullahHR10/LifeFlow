d8e428fa38a7dd40e8bad7afa35095e3f293a6a5
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-06-18 16:07:08 +0300

    Add extensions for the config class

---
+++ taskflow/backend/extensions.py
@@ -0,0 +1,8 @@
+"""Module that contains extensions for the config class."""
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
