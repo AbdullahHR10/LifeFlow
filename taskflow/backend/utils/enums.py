e26c086344782ea56cf2065a81a83d6daf71167b
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-06-19 18:03:56 +0300

    Add BackgroundColor, Priority and Category enums

---
+++ taskflow/backend/utils/enums.py
@@ -0,0 +1,31 @@
+"""Module that contains common enums."""
+from enum import Enum
+
+
+class BackgroundColor(Enum):
+    """Background color enum."""
+    BLUE = "Blue"
+    RED = "Red"
+    GREEN = "Green"
+    CYAN = "Cyan"
+    YELLOW = "Yellow"
+    ORANGE = "Orange"
+    PURPLE = "Purple"
+
+
+class Priority(Enum):
+    """Priority enum."""
+    LOW = "Low"
+    MEDIUM = "Medium"
+    HIGH = "High"
+    CRITICAL = "Critical"
+
+
+class Category(Enum):
+    """Category enum."""
+    WORK = "Work"
+    PERSONAL = "Personal"
+    STUDY = "Study"
+    HEALTH = "Health"
+    HOBBY = "Hobby"
+    OTHER = "Other"
