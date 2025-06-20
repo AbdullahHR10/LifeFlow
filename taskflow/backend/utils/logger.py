f15008c4b12c54ebd2664f6d2283920639856c2e
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-06-20 16:23:48 +0300

    Add advancded settings to the logger

--- taskflow/backend/utils/logger.py
+++ taskflow/backend/utils/logger.py
@@ -1,10 +1,29 @@
 """Module that contains the logger."""
 import logging
+import os
+from datetime import datetime
 
+log_dir = os.path.join(os.path.dirname(__file__), "..", "logs")
+os.makedirs(log_dir, exist_ok=True)
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s - %(levelname)s - %(message)s"
-    )
+log_file_name = f"{datetime.now().strftime('%Y-%m-%d')}.log"
+log_file_path = os.path.join(log_dir, log_file_name)
+
+formatter = logging.Formatter(
+    "%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
 
 logger = logging.getLogger("TaskFlowLogger")
+logger.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
