48043eb60bce1e445cf2e66053be0c521e5b08bc
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-06-27 19:04:57 +0300

    Add TaskSchema

---
+++ taskflow/backend/schemas/task_schema.py
@@ -0,0 +1,18 @@
+"""Module that contains the Task schema."""
+from marshmallow import Schema, fields, validate
+from ..utils.enums import Priority, Category
+
+
+class TaskSchema(Schema):
+    """Class that defines the schema of Task."""
+    title = fields.Str(required=True, validate=validate.Length(min=1))
+    description = fields.Str(required=True)
+    priority = fields.Str(
+        validate=validate.OneOf([prio.name for prio in Priority])
+    )
+    deadline = fields.DateTime(required=True)
+    completed = fields.Bool(required=False)
+    category = fields.Str(
+        validate=validate.OneOf([category.name for category in Category])
+    )
+    completed_at = fields.DateTime(required=False)
