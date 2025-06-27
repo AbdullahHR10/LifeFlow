d4607dda47c4b39791c5a0326c64f6809805601d
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-06-27 17:42:40 +0300

    Add sanitize_input that Sanitizes input fields using bleach and support for schemas in build_object and edit_object functions

--- taskflow/backend/utils/db_helpers.py
+++ taskflow/backend/utils/db_helpers.py
@@ -3,6 +3,29 @@
 from flask import abort, request
 from flask_login import current_user
 from backend import db
+import bleach
+
+
+def sanitize_input(data: dict, keys: list[str]) -> dict:
+    """
+    Sanitizes input fields using bleach.
+
+    Args:
+        data (dict): data containing potentially unsafe user data.
+        keys (list[str]): The list that will be sanitized.
+
+    Returns:
+        Dictionary with the specified keys sanitized.
+    """
+    sanitized = {}
+    for key in keys:
+        value = data.get(key)
+        if isinstance(value, str):
+            sanitized[key] = bleach.clean(value)
+        else:
+            sanitized[key] = value
+
+    return sanitized
 
 
 def get_object(model: type, obj_id: str) -> object:
@@ -24,7 +47,7 @@ def get_object(model: type, obj_id: str) -> object:
     return obj
 
 
-def build_object(model: type, keys: list[str]) -> object:
+def build_object(model: type, keys: list[str], schema=None) -> object:
     """
     Builds an instance of the given model using JSON data from the request.
 
@@ -37,12 +60,26 @@ def build_object(model: type, keys: list[str]) -> object:
         An instance of `model` with the extracted data and user id.
     """
     data = request.get_json()
-    obj_data = {key: data.get(key) for key in keys}
+
+    if schema:
+        from marshmallow import ValidationError
+        try:
+            obj_data = schema.load(data)
+        except ValidationError as e:
+            abort(404, description={
+                "status": "error",
+                "message": "Validation failed",
+                "data": e.messages
+            }), 400
+    else:
+        obj_data = {key: data.get(key) for key in keys}
+
+    obj_data = sanitize_input(obj_data, keys)
     obj_data["user_id"] = current_user.id
     return model(**obj_data)
 
 
-def edit_object(obj: object, keys: list[str]) -> object:
+def edit_object(obj: object, keys: list[str], schema=None) -> object:
     """
     Updates the provided object's attributes using JSON data from the request.
 
@@ -54,6 +91,16 @@ def edit_object(obj: object, keys: list[str]) -> object:
         object: The updated object instance with modified fields.
     """
     data = request.get_json()
+
+    if schema:
+        from marshmallow import ValidationError
+        try:
+            schema.load(data, partial=True)
+        except ValidationError as e:
+            abort(400, {"status": "error",
+                        "message": "Validation failed", "data": e.messages})
+
+    data = sanitize_input(data, keys)
     for key in keys:
         if key in data:
             setattr(obj, key, data[key])
