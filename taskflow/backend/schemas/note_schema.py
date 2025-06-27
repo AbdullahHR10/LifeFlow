08da814c1002ff957f4cbb9d4c8e526897292d57
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-06-27 18:03:55 +0300

    Add NoteSchema

---
+++ taskflow/backend/schemas/note_schema.py
@@ -0,0 +1,12 @@
+"""Module that contains the Note schema."""
+from marshmallow import Schema, fields, validate
+from ..utils.enums import BackgroundColor
+
+
+class NoteSchema(Schema):
+    """Class that defines the schema of Note."""
+    title = fields.Str(required=True, validate=validate.Length(min=1))
+    content = fields.Str(required=True)
+    background_color = fields.Str(
+        validate=validate.OneOf([color.name for color in BackgroundColor])
+    )
