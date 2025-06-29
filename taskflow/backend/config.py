43d9be756db8270912305927c23b0ab35479ba6e
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-06-29 16:35:18 +0300

    Add Redis configuration

--- taskflow/backend/config.py
+++ taskflow/backend/config.py
@@ -1,6 +1,6 @@
 """Module that contains the Config class."""
 import os
-import logging
+from redis import Redis
 
 
 class Config():
@@ -8,6 +8,14 @@ class Config():
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SESSION_TYPE = 'redis'
+    SESSION_PERMANENT = False
+    SESSION_USE_SIGNER = True
+    SESSION_KEY_PREFIX = 'taskflow:'
+    SESSION_REDIS = Redis(
+        host=os.environ.get("REDIS_HOST", "localhost"),
+        port=int(os.environ.get("REDIS_PORT", 6379))
+    )
 
 
 class TestConfig():
@@ -16,3 +24,7 @@ class TestConfig():
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "testing-secret-key"
+    RATELIMIT_STORAGE_URL = "memory://"
+    SESSION_TYPE = "filesystem"
+    SESSION_PERMANENT = False
+    SESSION_KEY_PREFIX = "taskflow_test:"
