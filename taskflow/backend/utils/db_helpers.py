2ca9ed0eb70bcd13575fd261a543a70d35d59bc1
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-07-10 15:50:32 +0300

    Add more options to db helpers

--- taskflow/backend/utils/db_helpers.py
+++ taskflow/backend/utils/db_helpers.py
@@ -4,6 +4,7 @@ from flask_login import current_user
 from backend import db
 from backend.models import Task, Habit, Budget, Transaction, Note
 import bleach
+from marshmallow import Schema
 
 
 def check_model(model: type):
@@ -55,51 +56,67 @@ def get_object(model: type, obj_id: str) -> object:
     return obj
 
 
-def build_object(model: type, keys: list[str], schema=None) -> object:
+def build_object(
+    model: type,
+    keys: list[str],
+    schema: Schema = None,
+    data: dict = None,
+    user_id: str = None
+) -> object:
     """
     Builds an instance of the given model using JSON data from the request.
 
     Args:
         model (type): The model class to instantiate.
         keys (list[str]): List of keys to extract from the JSON payload.
-        user_id (str): The user ID to include in the created object.
+        schema (Schema): The schema to test the data with.
+        data (dict): The data to build the object with.
+        user_id (str): The user's id.
 
     Returns:
         An instance of `model` with the extracted data and user id.
     """
     check_model(model)
-    data = request.get_json()
+    if data is None:
+        data = request.get_json()
 
     if schema:
         from marshmallow import ValidationError
         try:
             obj_data = schema.load(data)
         except ValidationError as e:
-            abort(404, description={
+            abort(400, description={
                 "status": "error",
                 "message": "Validation failed",
                 "data": e.messages
-            }), 400
+            })
     else:
         obj_data = {key: data.get(key) for key in keys}
 
     obj_data = sanitize_input(obj_data, keys)
-    obj_data["user_id"] = current_user.id
+    obj_data["user_id"] = user_id or current_user.id
     return model(**obj_data)
 
 
-def edit_object(obj: object, keys: list[str], schema=None) -> object:
+def edit_object(
+    obj: object,
+    keys: list[str],
+    schema: Schema = None,
+    data: dict = None
+) -> object:
     """
     Updates the provided object's attributes using JSON data from the request.
 
     Args:
         obj (object): The SQLAlchemy model instance to be edited.
         keys (list[str]): List of keys to extract from the JSON payload.
-
+        schema (Schema): The schema to test the data with.
+        data (dict): The data to build the object with.
     Returns:
         object: The updated object instance with modified fields.
     """
-    data = request.get_json()
+    if data is None:
+        data = request.get_json()
 
     if schema:
         from marshmallow import ValidationError
